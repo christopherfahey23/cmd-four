938d0745f57a7a7a90eafff760a109fcfd8a8e79
christopherfahey23 <christopherfahey23@gmail.com>
2022-01-17 14:47:09 -0500

    basic input handling

--- src/CmdFour.java
+++ src/CmdFour.java
@@ -1,8 +1,38 @@
+import java.util.Scanner;
+
 public class CmdFour {
-    public static Board board = new Board();
+    private static Board board;
+
+    private static boolean quit; 
     public static void main(String[] args) throws Exception {
-        board.print(System.out);
+        board = new Board();
+        quit = false;
+        readInput();
     }
 
+    private static void readInput() {
+        Scanner input = new Scanner(System.in);
 
+        while (!quit) {
+            String cmd = input.nextLine();
+            
+            if (cmd.equals("quit")) {
+                quit = true;
+            } else {
+                try {
+                    int move = Integer.parseInt(cmd);
+                    if (move >= 1 && move <= 7) {
+                        // TODO: call method in Board
+                        // board.takeTurn(move);
+                        
+                        System.out.println("Row " + move);
+                    } else {
+                        throw new RuntimeException();
+                    }
+                } catch(Exception e) {
+                    System.out.println("Invalid input. Please try again.");
+                }
+            }
+        }
+    }
 }
